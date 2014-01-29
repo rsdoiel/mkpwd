bcdd207a6b09b126dbfae98c265ffffdc0a893d8
Oliver Schroeder <gitorious@postrobot.de>
2014-01-29 12:51:28 +0100

    added autoconf support

---
+++ autogen.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+OSTYPE=`uname -s`
+MACHINE=`uname -m`
+AUTO_MAKE_VERSION=`automake --version | head -1 | awk '{print $4}' | sed -e 's/\.\([0-9]*\).*/\1/'`
+
+echo "Host info: $OSTYPE $MACHINE"
+echo -n " automake: `automake --version | head -1 | awk '{print $4}'`"
+echo " ($AUTO_MAKE_VERSION)"
+echo ""
+
+if [ ! -d m4 ]; then
+	mkdir m4
+fi
+
+echo "Running aclocal"
+aclocal -I m4
+if [ $? != 0 ]; then
+        echo 
+        echo "#"
+        echo "failed to call aclocal"
+        echo "You need autoconf and automake to build the sources"
+        echo "#"
+        echo 
+        exit
+fi
+
+#echo "Running libtoolize"
+#libtoolize --force --copy
+if [ $? != 0 ]; then
+        echo 
+        echo "#"
+        echo "failed to call libtoolize"
+        echo "You need libtool to build the sources"
+        echo "#"
+        echo 
+        exit
+fi
+
+echo "Running autoheader"
+autoheader
+if [ ! -e config.h.in ]; then
+        echo "ERROR: autoheader didn't create simgear/simgear_config.h.in!"
+        exit 1
+fi
+
+if [ ! -f AUTHORS ]; then
+	touch AUTHORS
+fi
+if [ ! -f NEWS ]; then
+	touch NEWS
+fi
+
+echo "Running automake --add-missing"
+automake --add-missing
+
+echo "Running autoconf"
+autoconf
+if [ ! -e configure ]; then
+        echo "ERROR: configure was not created!"
+        exit 1
+fi
+
+echo ""
+echo "======================================"
+if [ -f config.cache ]; then
+        echo "config.cache exists.  Removing the config.cache file will force"
+        echo "the ./configure script to rerun all it's tests rather than using"
+        echo "the previously cached values."
+        echo ""
+fi
+
+echo "Now you are ready to run './configure'"
+echo "======================================"
+
+
